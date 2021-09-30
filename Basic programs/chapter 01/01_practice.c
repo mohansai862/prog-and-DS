fe35399f41df41f0b284d6f2503616b39d7f0633
mohansai862 <91048177+mohansai862@users.noreply.github.com>
2021-09-30 22:25:34 +0530

    one file modified

--- Basic programs/chapter 01/01_practice.c
+++ Basic programs/chapter 01/01_practice.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int length = 8, breadth = 4;
+    int length = 88, breadth = 4;
     int area = length * breadth;
     printf("the area of the rectangle is %d", area);
     return 0;
