ea90e6d1d1c197071a5ac9edf0318a476fc4d9a0
mohansai862 <91048177+mohansai862@users.noreply.github.com>
2021-10-03 22:03:26 +0530

    loops

---
+++ Basic programs/chapter 04/04_01_loop_intro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello ");
+    printf("world\n");
+
+    int a = 1;
+    // Loops are used to repeat similar part of code snippet efficiently
+    // (         
+    // printf("%d\n", a);
+    // a++;) =--> 100 times
+
+    return 0;
+}
+   
+   
---
+++ Basic programs/chapter 04/04_02_while_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // float a;
+    int a;
+    scanf("%d", &a);
+
+    while (a < 10)
+    {
+        printf("%d\n", a);
+        a++;
+    }
+    return 0;
+}
